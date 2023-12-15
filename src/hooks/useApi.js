14d4d3b0794d6cca0f6da882fe65e3e1dbe0ccf2
Adiar <93129221+Okarix@users.noreply.github.com>
2023-12-15 22:07:45 +0600

    added hook: useApi

---
+++ src/hooks/useApi.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useApi = (url, method, data = null) => {
+	const [response, setResponse] = useState(null);
+	const [error, setError] = useState('');
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await axios({ url, method, data });
+				setResponse(res.data);
+			} catch (error) {
+				setError(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, [url, method, data]);
+
+	return { response, error, loading };
+};
+
+export default useApi;
